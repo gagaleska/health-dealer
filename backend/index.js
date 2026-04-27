40d808ab99c391dfd7b7a8e7053c9370c1126eb1
evgenija_gagaleska <evgenijagagaleskax@gmail.com>
2026-04-27 12:38:32 +0200

    Correction in the setup of the  node server using express

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 require("dotenv").config();
-const DB = require("./db/dbConn.js");
+const db = require("./db/dbConn.js");
 
 const cors = require("cors");
 const app = express();
